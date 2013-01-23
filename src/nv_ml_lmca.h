06b6586905e26d09b072d5bf0bf67b595dcd8a31
nagadomi <nagadomi@nurs.or.jp>
2013-01-23 09:48:46 +0900

    add comment

--- src/nv_ml_lmca.h
+++ src/nv_ml_lmca.h
@@ -22,9 +22,10 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+	
 typedef enum {
-	NV_LMCA_DIAG,
-	NV_LMCA_FULL
+	NV_LMCA_DIAG, /* only diagonal element */
+	NV_LMCA_FULL  /* full matrix */
 } nv_lmca_type_e;
 
 void nv_lmca_progress(int onoff);
