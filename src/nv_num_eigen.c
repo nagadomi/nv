3a6699fa3604f6bc8cbfcc40284e5a7fb1a47e8e
nagadomi <nagadomi@nurs.or.jp>
2013-06-07 17:59:06 +0900

    nv_eigenをベクトル命令化

--- src/nv_num_eigen.c
+++ src/nv_num_eigen.c
@@ -33,6 +33,10 @@ nv_eigen(nv_matrix_t *eigen_vec,
 {
 	int i;
 	nv_matrix_t *a = nv_matrix_dup(mat);
+	nv_matrix_t *vec_tmp = nv_matrix_alloc(a->m, 1);
+#if NV_ENABLE_SSE2	
+	const int pk_lp = (a->n & 0xfffffffc);
+#endif
 	
 	NV_ASSERT(n > 0);
 	NV_ASSERT(n <= mat->m);
@@ -51,7 +55,6 @@ nv_eigen(nv_matrix_t *eigen_vec,
 		
 		for (k = 0; k < max_epoch; ++k) {
 			int j;
-			nv_matrix_t *vec_tmp = nv_matrix_alloc(a->m, 1);
 			float lambda;
 			
 #ifdef _OPENMP
@@ -66,7 +69,6 @@ nv_eigen(nv_matrix_t *eigen_vec,
 			}
 			NV_MAT_V(eigen_val, i, 0) = lambda;
 			nv_vector_copy(eigen_vec, i, vec_tmp, 0);
-			nv_matrix_free(&vec_tmp);
 			
 			if (k > 0) {
 				if (fabsf(lambda_old - lambda) < FLT_EPSILON) {
@@ -75,7 +77,30 @@ nv_eigen(nv_matrix_t *eigen_vec,
 			}
 			lambda_old = NV_MAT_V(eigen_val, i, 0);
 		}
-		
+#if NV_ENABLE_SSE2
+		{
+			const __m128 val = _mm_set1_ps(NV_MAT_V(eigen_val, i, 0));
+			
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+			for (jj = 0; jj < a->m; ++jj) {
+				int ii;
+				const __m128 vjj = _mm_set1_ps(NV_MAT_V(eigen_vec, i, jj));
+				for (ii = 0; ii < pk_lp; ii += 4) {
+					_mm_store_ps(&NV_MAT_V(a, jj, ii),
+								 _mm_sub_ps(*(const __m128 *)&NV_MAT_V(a, jj, ii),
+											_mm_mul_ps(val,_mm_mul_ps(vjj, *(const __m128 *)&NV_MAT_V(eigen_vec, i, ii)))));
+				}
+				for (; ii < a->n; ++ii) {
+					NV_MAT_V(a, jj, ii) -=
+						NV_MAT_V(eigen_val, i, 0)
+						* NV_MAT_V(eigen_vec, i, ii)
+						* NV_MAT_V(eigen_vec, i, jj);
+				}
+			}
+		}
+#else
 #ifdef _OPENMP
 #pragma omp parallel for
 #endif
@@ -88,7 +113,9 @@ nv_eigen(nv_matrix_t *eigen_vec,
 					* NV_MAT_V(eigen_vec, i, jj);
 			}
 		}
+#endif		
 	}
+	nv_matrix_free(&vec_tmp);
 	nv_matrix_free(&a);
 	
 	return 0;
