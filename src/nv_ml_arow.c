4a522d1b2aea09e4b14ee816c99202ceb8b7bdc4
nagadomi <nagadomi@nurs.or.jp>
2014-01-22 23:29:57 +0900

    add support for bias in nv_arow

--- src/nv_ml_arow.c
+++ src/nv_ml_arow.c
@@ -42,6 +42,7 @@ nv_arow_alloc(int n, int k)
 	} else {
 		arow->w = nv_matrix_alloc(arow->n, k);
 	}
+	arow->bias = nv_matrix_alloc(1, k);
 	
 	return arow;
 }
@@ -50,6 +51,7 @@ void
 nv_arow_init(nv_arow_t *arow)
 {
 	nv_matrix_zero(arow->w);
+	nv_matrix_zero(arow->bias);
 }
 
 void
@@ -57,6 +59,7 @@ nv_arow_free(nv_arow_t **arow)
 {
 	if (arow && *arow) {
 		nv_matrix_free(&(*arow)->w);
+		nv_matrix_free(&(*arow)->bias);
 		nv_free(*arow);
 		*arow = NULL;
 	}
@@ -70,15 +73,16 @@ nv_arow_train_at(nv_arow_t *arow,
 				 float r,
 				 int max_epoch)
 {
-#if NV_ENABLE_SSE2
+#if NV_ENABLE_SSE
 	const int pk_lp = (arow->n & 0xfffffffc);
 	const __m128 o = _mm_set1_ps(1.0f);
 	const __m128 b = _mm_set1_ps(1.0f / r);
 #endif
 	int epoch;
-	nv_matrix_t *cov;
-
-	cov = nv_matrix_alloc(arow->n, 1);
+	nv_matrix_t *cov = nv_matrix_alloc(arow->n, 1);
+	float covb = 1.0f;
+	const float r_inv = 1.0f / r;
+	
 	nv_matrix_fill(cov, 1.0f);
 	
 	for (epoch = 0; epoch < max_epoch; ++epoch) {
@@ -90,10 +94,10 @@ nv_arow_train_at(nv_arow_t *arow,
 			int i;
 			int rand_j = nv_rand_index(data->m);
 			int y = NV_MAT_VI(labels, rand_j, 0) == posi_label ? 1:-1;
-			float xu = nv_vector_dot(arow->w, posi_label, data, rand_j);
+			float xu = nv_vector_dot(arow->w, posi_label, data, rand_j) + NV_MAT_V(arow->bias, posi_label, 0);
 			float alpha = 1.0f - y * xu;
 			float var;
-#if NV_ENABLE_SSE2
+#if NV_ENABLE_SSE
 			NV_ALIGNED(float, mm[4], 16);
 			__m128 a, c, d, u;
 #endif
@@ -103,7 +107,7 @@ nv_arow_train_at(nv_arow_t *arow,
 				continue;
 			}
 			
-#if NV_ENABLE_SSE2
+#if NV_ENABLE_SSE
 			u = _mm_setzero_ps();
 			for (i = 0; i < pk_lp; i += 4) {
 				d = _mm_load_ps(&NV_MAT_V(data, rand_j, i));
@@ -111,7 +115,7 @@ nv_arow_train_at(nv_arow_t *arow,
 				u = _mm_add_ps(u, _mm_mul_ps(_mm_mul_ps(d, d), c));
 			}
 			_mm_store_ps(mm, u);
-			var = mm[0] + mm[1] + mm[2] + mm[3];
+			var = covb + mm[0] + mm[1] + mm[2] + mm[3];
 			for (i = pk_lp; i < arow->n; ++i) {
 				var += NV_MAT_V(data, rand_j, i) * NV_MAT_V(data, rand_j, i)
 					* NV_MAT_V(cov, 0, i);
@@ -141,10 +145,12 @@ nv_arow_train_at(nv_arow_t *arow,
 				NV_MAT_V(cov, 0, i) =
 					1.0f / (1.0f / NV_MAT_V(cov, 0, i) +
 							(NV_MAT_V(data, rand_j, i) 
-							 * NV_MAT_V(data, rand_j, i) * (1.0f / r)));
+							 * NV_MAT_V(data, rand_j, i) * r_inv));
 			}
+			NV_MAT_V(arow->bias, posi_label, 0) += alpha * covb;
+			covb = 1.0f / ((1.0f / covb) + r_inv);
 #else
-			var = 0.0f;
+			var = covb;
 			for (i = 0; i < arow->n; ++i) {
 				var += NV_MAT_V(data, rand_j, i) * NV_MAT_V(data, rand_j, i) *
 					NV_MAT_V(cov, 0, i);
@@ -158,6 +164,8 @@ nv_arow_train_at(nv_arow_t *arow,
 							(NV_MAT_V(data, rand_j, i) 
 							 * NV_MAT_V(data, rand_j, i) * (1.0f / r)));
 			}
+			NV_MAT_V(arow->bias, posi_label, 0) += alpha * covb;
+			covb = 1.0f / ((1.0f / covb) + r_inv);
 #endif
 		}
 		if (nv_arow_progress_flag) {
@@ -180,7 +188,7 @@ nv_arow_train(nv_arow_t *arow,
 	} else {
 		int k;
 #ifdef _OPENMP
-#pragma omp	parallel for
+#pragma omp	parallel for schedule(dynamic, 1)
 #endif
 		for (k = 0; k < arow->k; ++k) {
 			nv_arow_train_at(arow, data, labels, k, r, max_epoch);
@@ -193,13 +201,14 @@ nv_arow_predict_label(const nv_arow_t *arow,
 					  const nv_matrix_t *vec, int j)
 {
 	if (arow->n == 2) {
-		return nv_vector_dot(arow->w, 0, vec, j) >= 0.0f ? 0 : 1;
+		float y = nv_vector_dot(arow->w, 0, vec, j) + NV_MAT_V(arow->bias, 0, 0);
+		return y >= 0.0f ? 0 : 1;
 	} else {
 		int max_k = -1;
 		float max_dot = -FLT_MAX;
 		int k;
 		for (k = 0; k < arow->k; ++k) {
-			float dot = nv_vector_dot(arow->w, k, vec, j);
+			float dot = nv_vector_dot(arow->w, k, vec, j) + NV_MAT_V(arow->bias, 0, 0);
 			if (dot > max_dot) {
 				max_dot = dot;
 				max_k = k;
